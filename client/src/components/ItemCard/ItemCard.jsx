a5a501c17da1aaffb14edd851aa0893af3f5a438
alfenn <52635707+alfenn@users.noreply.github.com>
2023-01-29 13:38:18 -0600

    lay out page navigation logic with corresponding mock data

--- client/src/components/ItemCard/ItemCard.jsx
+++ client/src/components/ItemCard/ItemCard.jsx
@@ -1,11 +1,18 @@
 // const ItemCard = ({ locationData, itemData }) => {
-const ItemCard = ({ itemData }) => {
+const ItemCard = ({ itemData, reviewsData }) => {
+  const avgRating = Object.entries(reviewsData).filter(([key, value]) => (
+      Object.values(itemData.reviews).includes(key))
+    ).map(([key, value]) => (value.rating)).reduce((a, b) => (a + b), 0)
+    / (Object.entries(reviewsData).filter(([key, value]) => (
+        Object.values(itemData.reviews).includes(key)
+      )).length);
   return (
     // <a href={`${locationData.name}/${itemData.name}`}>
-      <div style={{ "border": "black solid 1px" }}>
-        <h1>{itemData.name}</h1>
-        <p>{itemData.description}</p>
-      </div>
+    <div className="m-3" style={{ "border": "black solid 1px" }}>
+      <h1>{itemData.name}</h1>
+      <p>{itemData.description}</p>
+      <p>{avgRating} stars</p>
+    </div>
     // </a>
   );
 }
--- client/src/pages/LocationPage.jsx
+++ client/src/pages/LocationPage.jsx
@@ -2,25 +2,28 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ItemCard from '../components/ItemCard/ItemCard';
+import { Link } from 'react-router-dom';
 
-const LocationPage = ({ locationData, itemsData, reviewData }) => {
+const LocationPage = ({ locationData, itemsData, reviewsData }) => {
   return (
     <div className="App">
       <Container fluid="md">
         <Row>
-          <Col>
+          <Col className='m-5'>
             <h1>
               {locationData.name}
             </h1>
+            <Link to="/">Return to locations</Link>
           </Col>
         </Row>
         <Row>
           <Col>
-            {itemsData.map(([key, value]) => (<ItemCard key={key} 
-              itemData={value} 
+            {itemsData.map(([key, value]) => (
+              <Link key={key} to={`/${key}`} style={{"textDecoration": "none", "color": "black"}}>
+                <ItemCard itemData={value} reviewsData={reviewsData}
               // reviewData={reviewData.filter(([key, value]) => (value.reviews.includes(key)))}
-              // locationData={locationData} 
-              />))}
+              // locationData={locationData}
+              /></Link>))}
           </Col>
         </Row>
       </Container>
