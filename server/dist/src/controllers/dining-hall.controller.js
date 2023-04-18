21bd437e22ec2636698905dedfe08f6bbed70a0b
Jackie Lin <jackielin2025@u.northwestern.edu>
2023-04-17 23:12:01 -0500

    dining hall query?

--- server/dist/src/controllers/dining-hall.controller.js
+++ server/dist/src/controllers/dining-hall.controller.js
@@ -2,10 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getDiningHall = void 0;
 const utils_1 = require("../database/utils");
+const mongodb_1 = require("mongodb");
 const getDiningHall = async (req, res) => {
     try {
         const diningHall = req.params.diningHallName;
-        const result = await (0, utils_1.find)(diningHall, { "diningHall": req.params.diningHallName });
+        const query = {};
+        if (req.query._id) {
+            query._id = new mongodb_1.ObjectId(req.query._id);
+        }
+        const result = await (0, utils_1.find)(diningHall, query);
         res.json(result);
     }
     catch (err) {
--- server/src/controllers/dining-hall.controller.ts
+++ server/src/controllers/dining-hall.controller.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from "express";
 import { find } from "../database/utils";
+import { ObjectId } from "mongodb";
 
 const getDiningHall = async(req: Request, res: Response) => {
   try {
-    const diningHall = req.params.diningHallName
+    const diningHall = req.params.diningHallName;
+
+    const query = {};
     
-    const result = await find(diningHall, {"diningHall" : req.params.diningHallName});
+    if (req.query._id) {
+      query._id = new ObjectId(req.query._id as string);
+    }
+
+    const result = await find(diningHall, query);
     res.json(result);
   }
   catch (err: any) {
